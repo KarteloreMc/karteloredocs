0b315147a8857575b864af9bbdc6267438f653f5
KarteloreMc <kartelore@gmail.com>
2023-07-24 20:27:25 +0200

    test

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -136,4 +136,17 @@ const config = {
     }),
 };
 
-module.exports = config;
+module.exports = {
+  // ...
+  presets: [
+    '@docusaurus/preset-classic',
+    {
+      docs: {
+        routeBasePath: '/docs/intro', // Serve the docs at the site's root
+        /* other docs plugin options */
+      },
+      blog: false, // Optional: disable the blog plugin
+      // ...
+    },
+  ],
+};
