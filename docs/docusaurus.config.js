eb8e98028aa70f80d605da62ee64c35929bc5a4a
KarteloreMc <kartelore@gmail.com>
2023-07-26 01:06:40 +0200

    url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.kartelore.pl/',
+  url: 'https://docs.kartelore.pl',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/karteloredocs/',
