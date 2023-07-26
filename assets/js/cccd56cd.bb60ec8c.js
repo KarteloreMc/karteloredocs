89da391d0e4d1af28f6f16cb6ff1b778e8214e15
KarteloreMc <kartelore@gmail.com>
2023-07-26 17:27:25 +0200

    Deploy website - based on HEAD

--- assets/js/cccd56cd.bb60ec8c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[246],{5619:a=>{a.exports=JSON.parse('{"title":"Kategoria2","slug":"/category/kategoria2","permalink":"/category/kategoria2","navigation":{"previous":{"title":"Strona6","permalink":"/poradnik/strona6"},"next":{"title":"Strona 7","permalink":"/kategoria2/strona7"}}}')}}]);
